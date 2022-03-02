50e753d934fc5c191aabe671cd9ab8f6c052a606
yaozaofeng <2412684394@qq.com>
2022-03-02 17:14:55 +0800

    新建一篇博客

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -1,23 +1,44 @@
 const getList = (author, keyword) => {
-    // 先返回假数据（格式是正确的）
-    return [
-        {
-            id: 1,
-            title: '标题A',
-            content: '内容A',
-            createTime: 1646204461414, 
-            author: '张三'
-        },
-        {
-            id: 2,
-            title: '标题B',
-            content: '内容B',
-            createTime: 1646204498787, 
-            author: '里斯'
-        },
-    ]
-}
+  // 先返回假数据（格式是正确的）
+  return [
+    {
+      id: 1,
+      title: "标题A",
+      content: "内容A",
+      createTime: 1646204461414,
+      author: "张三",
+    },
+    {
+      id: 2,
+      title: "标题B",
+      content: "内容B",
+      createTime: 1646204498787,
+      author: "里斯",
+    },
+  ];
+};
+
+const getDetail = (id) => {
+  // 先返回假数据
+  return {
+    id: 1,
+    title: "标题A",
+    content: "内容A",
+    createTime: 1646204461414,
+    author: "张三",
+  };
+};
+
+const newBlog = (blogData = {}) => {
+  // blogData 是一个博客对象，包含 title content 属性
+  
+  return {
+    id: 3, // 表示新建博客，插入到数据表里面的 id
+  };
+};
 
 module.exports = {
-    getList
-}
+  getList,
+  getDetail,
+  newBlog,
+};
--- src/router/blog.js
+++ src/router/blog.js
@@ -1,41 +1,40 @@
-const { getList } = require('../controller/blog')
-const { SuccessModel, ErrorModel } = require('../model/resModel')
+const { getList, getDetail, newBlog } = require("../controller/blog");
+const { SuccessModel, ErrorModel } = require("../model/resModel");
 
 const handleBlogRouter = (req, res) => {
   const method = req.method;
-  
+
   // 获取博客列表
   if (method === "GET" && req.path === "/api/blog/list") {
-    const author = req.query.author || ''
-    const keyword = req.query.keyword || ''
-    const listData = getList(author, keyword)
+    const author = req.query.author || "";
+    const keyword = req.query.keyword || "";
+    const listData = getList(author, keyword);
 
-    return new SuccessModel(listData)
+    return new SuccessModel(listData);
   }
 
   // 获取博客详情
-  if (method === "GET" && req.path === "/api/blog/list") {
-    return {
-      msg: "获取博客详情的接口",
-    };
+  if (method === "GET" && req.path === "/api/blog/detail") {
+    const id = req.query.id;
+    const data = getDetail(id);
+    return new SuccessModel(data);
   }
 
   // 新建一篇博客
-  if (method === "GET" && req.path === "/api/blog/new") {
-    return {
-      msg: "获取新建博客的接口",
-    };
+  if (method === "POST" && req.path === "/api/blog/new") {
+    const data = newBlog(req.body)
+    return new SuccessModel(data);
   }
 
-  // 更细一篇博客
-  if (method === "GET" && req.path === "/api/blog/update") {
+  // 更新一篇博客
+  if (method === "POST" && req.path === "/api/blog/update") {
     return {
       msg: "获取更新博客的接口",
     };
   }
 
   // 删除一篇博客
-  if (method === "GET" && req.path === "/api/blog/del") {
+  if (method === "POST" && req.path === "/api/blog/del") {
     return {
       msg: "获取删除博客的接口",
     };
